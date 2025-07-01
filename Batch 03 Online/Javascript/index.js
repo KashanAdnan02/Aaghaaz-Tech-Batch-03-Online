6cbc491d4feb1ee0010bea8f2e46c366a216a405
KashanAdnan02 <khanzaidaboy@gmail.com>
2025-07-01 08:53:21 -0700

    Javascript First Class

---
+++ Batch 03 Online/Javascript/index.js
@@ -0,0 +1,28 @@
+// alert("successfull!")
+
+// numbers
+// var a = 2
+
+// alert(a)
+
+// string
+// var name = "kashan"
+// alert(name)
+
+// + - / * ** %
+
+
+var num1 = 18
+var num2 = 8
+// * + - /    ** %
+
+// ** exponent
+// % modules
+// / Divide
+// * Multiply
+// + Plus
+// - Subtract
+
+// ** exponent
+var num3 = num1 % num2
+alert(num3)
