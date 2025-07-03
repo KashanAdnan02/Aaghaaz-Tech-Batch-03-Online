da516d6627e6839f11b63ef9def04647a0277f69
KashanAdnan02 <khanzaidaboy@gmail.com>
2025-07-03 09:32:32 -0700

    some-code

---
+++ Batch 03 Online/Javascript/class 2/index.js
@@ -0,0 +1,26 @@
+// BODMAS // Brackets Operators
+// 
+// var num1 = 9
+// var num2 = 2
+// 18
+//   18            + 7 25
+// alert((8 * 7) * (9 + 9))
+// 1008
+// 72
+// 24
+
+// Temperature Conversion
+// Declare a variable celsius and assign it a temperature value (e.g., 25).
+// Convert the temperature to Fahrenheit using the formula: fahrenheit = (celsius * 9/5) + 32.
+// Log the result to the console with a message like "25°C is equal to [fahrenheit]°F".
+
+// var celsius = 25
+
+
+
+// Prompt
+
+var celsius = prompt("Enter your celsius")
+var fahrenheit = (celsius * 9 / 5) + 32
+
+alert(fahrenheit)
---
+++ Batch 03 Online/Javascript/class 3/index.js
@@ -0,0 +1,32 @@
+// 100 === 100 // true
+// number === string // false
+// if (100 == "100") { // true
+//     // conditional operators ===, ==, <, >
+//     alert("If ke andar wala code chala")
+// } else {
+//     alert("else ke andar wala code chala") // 
+// }
+
+// var city = prompt("Enter your city: ")
+
+// if (city !== "karachi") {
+//     alert("It works")
+// }
+
+// if (city === "Lahore") {
+//     alert("You live in the good city")
+// }
+// else if (city === "Islamabad") {
+//     alert("You live in the capital of pakistan")
+// }
+// else {
+//     alert("You live in the greatest City")
+// }
+
+
+if (10 < 9) {
+    alert("Eight is less than 9")
+}
+else {
+    alert("it work")
+}
