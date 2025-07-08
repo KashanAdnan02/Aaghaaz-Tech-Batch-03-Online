e8cb77547406ed7e92792745cccd19c6e3457a12
KashanAdnan02 <khanzaidaboy@gmail.com>
2025-07-08 09:23:29 -0700

    Revision of variables and alert

---
+++ Batch 03 Online/Javascript/class 4/index.js
@@ -0,0 +1,13 @@
+// alert("Hello world")
+var name = "Kashan Adnan" // String {Data Type}
+var age = 13 // Number {Data Type}
+var message = "Hi, i am a developer!"
+// true, false {Boolean}
+
+var a = true // Boolean Or String
+
+
+var m = null;
+alert(m) // undefiend
+
+// + - / * 
