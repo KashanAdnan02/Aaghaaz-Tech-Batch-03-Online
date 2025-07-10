82589ba4f38f2508124663906f6234edf8310fdb
KashanAdnan02 <khanzaidaboy@gmail.com>
2025-07-10 07:16:51 -0700

    javasript

---
+++ Batch 03 Online/Javascript/class 5/index.js
@@ -0,0 +1,35 @@
+var num1 = 10
+var num2 = 10
+
+// <
+// 10 > 0 True
+
+// Single Condition && 
+
+
+//    num1 > num2    &&    8 === 9
+//      true 
+// && and and operator
+// || or or operator
+// true     // false
+// true     // false
+// false    // false
+/// 
+if (num1 <= num2 || 8 === 9) {
+    alert("It works")
+} else {
+    alert("Wrong")
+}
+var art = prompt("Enter any arthemtic operator")
+if (art === "*") {
+    alert(num1 * num2)
+}
+if (art === "*") {
+    alert(num1 * num2)
+}
+if (art === "*") {
+    alert(num1 * num2)
+}
+// It works
+// It works
+// It works
