5567258bc3be18f7c9a8cfe85d58b9c57bad1808
KashanAdnan02 <khanzaidaboy@gmail.com>
2025-07-13 07:22:24 -0700

    array

---
+++ Batch 03 Online/Javascript/class 6/index.js
@@ -0,0 +1,46 @@
+// varible me naam store karna he
+
+
+var name = "Kashan"
+var age = 13
+//                 0                            1                     2
+// var courses = ["Web & App Development", "Graphic Designing", "Ai & Datascience"]
+
+// 2 // 3//
+// alert(courses.at(2))
+// 3
+// 2
+// 2
+// 2 
+// alert(courses.join("*********"))
+// 1 
+// 2 
+// alert(courses[1])
+// alert(courses.length)
+
+
+// var fruits = ["apple", "mango", "kiwi", "avacado"]
+// console.log(fruits)
+
+// fruits.pop()
+// console.log(fruits)
+
+// var fruits = ["apple", "mango", "kiwi", "avacado"]
+// fruits.push("banana")
+// fruits.unshift("watermelon")
+// console.log(fruits)
+
+// var fruits = ["apple", "mango", "kiwi", "avacado"]
+
+// fruits.shift()
+// console.log(fruits)
+
+// var fruits = ["apple", "mango", "kiwi", "avacado"]
+
+// fruits[4] = "watermelon"
+
+// console.log(fruits)
+const fruits = ["Banana", "Orange", "Apple", "Mango"];
+fruits[fruits.length] = "Kiwi";
+delete fruits[fruits.length]
+console.log(fruits)
