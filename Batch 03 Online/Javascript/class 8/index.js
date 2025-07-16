07d5a0eb50af09166221b4883967a3415056f921
KashanAdnan02 <khanzaidaboy@gmail.com>
2025-07-16 12:47:41 -0700

    19 chapter to 24 chapter for loops flag slice toUpperCase toLowerCase indexOf lastIndexOf charAT

---
+++ Batch 03 Online/Javascript/class 8/index.js
@@ -0,0 +1,53 @@
+// For Loops
+// var cityToCheck = prompt("Enter your cleanest city")
+
+// cleanestCities = ["Karachi", "Islamabad", "Lahore"]
+// var matchFound = "no"
+// for (var i = 0; i < cleanestCities.length; i += 1) {
+//     if (cityToCheck === cleanestCities[i]) {
+//         matchFound = "yes"
+//         alert("It's one of the cleanest cities");
+//     }
+// }
+// if (matchFound === "no") {
+//     alert("It is wrong")
+// }
+
+
+// if (cityToCheck === cleanestCities[0]) {
+//     alert("It's one of the cleanest cities");
+// }
+
+// if (cityToCheck === cleanestCities[1]) {
+//     alert("It's one of the cleanest cities");
+// }
+
+
+// for (var i = 0; i <= 4; i++) {
+//     if (cityToCheck === cleanestCities[i]) {
+//         matchFound = "yes";
+//         alert("It's one of the cleanest cities");
+//     }
+// }
+
+
+// var title = "mackbook"
+// var title2 = "PHONE"
+// console.log(title.toUpperCase())
+// console.log(title2.toLowerCase())
+
+
+//              01234567890123456789212345678901234567890
+var text = "A quick brown fox jump over the lazy quick dog"
+// console.log(text.indexOf("kashan")) // 2
+// console.log(text.lastIndexOf("quick"))
+
+
+console.log(text.charAt(2) + "?")
+// var updated_text = text.splice(2, 13)
+
+// console.log(updated_text)
+// var sliced_sentece = sentence.slice(2, 13)
+// console.log(sliced_sentece)
+
+//https://wccftech.com/wp-content/uploads/2014/10/JavaScript.pdf
