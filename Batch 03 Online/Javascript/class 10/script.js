827562319ccf77725c973bff005ffafa7b95b117
KashanAdnan02 <khanzaidaboy@gmail.com>
2025-07-20 08:00:24 -0700

    somec-deo

---
+++ Batch 03 Online/Javascript/class 10/script.js
@@ -0,0 +1,17 @@
+// var num1 = "3000"
+
+// console.log(num1)
+// console.log(typeof num1)
+
+// // Number || parseInt
+
+// console.log(typeof parseInt(num1))
+// console.log(typeof Number(num1))
+
+
+// if (true) {
+// const a = "Kashan"
+// a = "Tanveer"
+// console.log(a)
+// }
+// console.log(a)
---
+++ Batch 03 Online/Javascript/class 11/script.js
@@ -0,0 +1,13 @@
+// Changing Case
+
+// UPPER CASE
+// lower case
+var name1 = "KASHAN" // Upper Case
+var name2 = "kashan" // Lower Case
+
+
+if (name1.toLowerCase() == name2.toLowerCase()) {
+    alert("It is successfull!");
+}
+
+
