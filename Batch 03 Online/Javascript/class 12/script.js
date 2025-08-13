6b6d4a07319e3d4bf13020d80bebcabace6016a7
KashanAdnan02 <khanzaidaboy@gmail.com>
2025-08-13 14:09:26 -0700

    done

---
+++ Batch 03 Online/Javascript/class 12/script.js
@@ -0,0 +1,13 @@
+
+// console.log();
+
+// document.getElementsByTagName("body")[0].lastChild.style.backgroundColor = "green"
+
+
+function submit() {
+    // for (let i = 0; i < 9; i++) {
+    //     console.log(i);
+    // }
+    console.log("abc");
+
+}
